c4c65f1cb6f6c57c8d8cb635f8047ebaf59b61eb
Justin Salamon <justin.salamon@gmail.com>
2017-10-10 18:02:11 -0400

    Remove self dependency

--- skm/display.py
+++ skm/display.py
@@ -4,7 +4,7 @@ import matplotlib.cm as cmx
 import matplotlib.colors as colors
 
 # REMOVE PYLAB DEPENDENCY
-def _get_cmap(self, N):
+def _get_cmap(N):
     '''Returns a function that maps each index in 0, 1, ... N-1 to a distinct
     RGB color.'''
     color_norm  = colors.Normalize(vmin=0, vmax=N-1)
@@ -14,26 +14,26 @@ def _get_cmap(self, N):
     return map_index_to_rgb_color
 
 
-def visualize_clusters(self, X):
+def visualize_clusters(s, X):
     '''
-    Visualize the input data and cluster assignments
+    Visualize the input data X and cluster assignments from SKM model s.
     '''
     plt.figure(figsize=(8,8))
     # colors_ = ['b','r','g', 'c', 'm', 'y', 'k', 'DeepPink', 'Lime', 'Maroon', 'b','r','g', 'c', 'm', 'y', 'k', 'DeepPink', 'Lime', 'Maroon']
-    cmap = self._get_cmap(self.k)
+    cmap = s._get_cmap(s.k)
     for n,point in enumerate(X.T):
         # point = self.pca.inverse_transform(point.T)
-        if self.assignment is None:
+        if s.assignment is None:
             col = 'b'
         else:
-            col = cmap(self.assignment[n])
+            col = cmap(s.assignment[n])
         plt.plot(point[0],point[1], 'o', color=col)
         plt.axis([-2, 2, -2, 2])
     # plt.axhline(0, color='black')
     # plt.axvline(0, color='black')
-    for n,centroid in enumerate(self.D.T):
+    for n,centroid in enumerate(s.D.T):
         # centroid = self.pca.inverse_transform(centroid.T)
-        if self.assignment is None:
+        if s.assignment is None:
             col = 'b'
         else:
             col = cmap(n)
